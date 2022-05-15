f1bb8be8897641ff0279fe6412a2b7be56ebbf47
qsbugsy <49853219+qsbugsy@users.noreply.github.com>
2022-05-14 20:31:29 -0400

    first commmit

---
+++ frontend/src/data.js
@@ -0,0 +1,70 @@
+const data = {
+    products= [
+        {
+            _id: '1',
+            name: 'white air force 1',
+            category: 'sneakers',
+            image: './images/img9.jpg',
+            price:'$100',
+            brand: 'Nike',
+            rating: 5,
+            numReviews: 10,
+            description: 'high quaity product'
+        },
+        {
+            _id: '2',
+            name: 'red air max',
+            category: 'sneakers',
+            image: './images/img10.jpg',
+            price:'$110',
+            brand: 'Nike',
+            rating: 5,
+            numReviews: 13,
+            description: 'high quaity product'
+        },
+        {
+            _id: '3',
+            name: 'yellow nike',
+            category: 'sneakers',
+            image: './images/img11.jpg',
+            price:'$100',
+            brand: 'Nike',
+            rating: 5,
+            numReviews: 10,
+            description: 'high quaity product'
+        },
+        {
+            _id: '4',
+            name: 'vans',
+            category: 'sneakers',
+            image: './images/img12.jpg',
+            price:'$100',
+            brand: 'Vans',
+            rating: 5,
+            numReviews: 10,
+            description: 'high quaity product'
+        },
+        {
+            _id: '5',
+            name: 'red presto',
+            category: 'sneakers',
+            image: './images/img13.jpg',
+            price:'$90',
+            brand: 'Nike',
+            rating: 4,
+            numReviews: 10,
+            description: 'high quaity product'
+        },
+        {
+            _id: '6',
+            name: 'black boots',
+            category: 'sneakers',
+            image: './images/img14.jpg',
+            price:'$100',
+            brand: 'Nike',
+            rating: 5,
+            numReviews: 10,
+            description: 'high quaity product'
+        }
+    ]
+}
