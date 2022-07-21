52a24859ed2907c4c8dcbf1230388fa00076d74e
qsbugsy <49853219+qsbugsy@users.noreply.github.com>
2022-07-21 18:39:00 -0400

    added sample data

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import data from './data.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import seedRouter from './routes/seedRoutes.js';
+import productRouter from './routes/productRoutes.js';
 
 dotenv.config();
 mongoose.connect(process.env.MONGODB_URI).then(()=>{
@@ -14,7 +16,9 @@ const app = express();
 
 app.get('/',(req,res) =>{
     res.send('Server is ready');
-})
+});
+app.use('/api/seed', seedRouter);
+app.use('/api/products', productRouter);
 const port = process.env.PORT || 5002;
 
 app.listen(port, () => {
